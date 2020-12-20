d6f6be59d435b28020fcacd4770b6bcc0d7f3265
AlexAvizov <alex.avizov@sap.com>
2020-12-20 21:25:31 +0200

    add contacts class

---
+++ src/contacts.ts
@@ -0,0 +1,69 @@
+import * as vscode from 'vscode';
+import * as fs from 'fs';
+import * as path from 'path';
+
+export class contactsListProvider implements vscode.TreeDataProvider<Contacts> {
+
+	private _onDidChangeTreeData: vscode.EventEmitter<Contacts | undefined | void> = new vscode.EventEmitter<Contacts | undefined | void>();
+	readonly onDidChangeTreeData: vscode.Event<Contacts | undefined | void> = this._onDidChangeTreeData.event;
+
+	constructor(private workspaceRoot: string) {
+	}
+
+	refresh(): void {
+		this._onDidChangeTreeData.fire();
+	}
+
+	getTreeItem(element: Contacts): vscode.TreeItem {
+		return element;
+	}
+
+	getChildren(element?: Contacts): Thenable<Contacts[]> {
+		if (!this.workspaceRoot) {
+			vscode.window.showInformationMessage('No contacts in empty workspace');
+			return Promise.resolve([]);
+		}
+
+		if (element) {
+			return Promise.resolve(this.getContactsList(path.join(this.workspaceRoot, element.contactId)));
+		} else {
+			return Promise.resolve(this.getContactsList(path.join(this.workspaceRoot)));			
+		}
+	}
+
+	/**
+	 * Given the path to package.json, read all its dependencies and devDependencies.
+	 */
+	private getContactsList(contactId: string): Contacts[] {
+        const contact1 = new Contacts("1", "Alex", "Alex Avizov", "alex.avizov@sap.com", vscode.TreeItemCollapsibleState.Collapsed);
+        const contact2 = new Contacts("2", "Shimi", "Shimon Tal", "shimon.tal@sap.com", vscode.TreeItemCollapsibleState.Collapsed);
+        let contactsList : Contacts[] = [];
+        contactsList.push(contact1);        
+        contactsList.push(contact2);
+        return contactsList;				
+	}
+}
+
+export class Contacts extends vscode.TreeItem {
+
+	constructor(
+		public readonly contactId: string,
+        public readonly contactName: string,
+        public readonly contactFullName: string,
+        public readonly contactEmail: string,
+		public readonly collapsibleState: vscode.TreeItemCollapsibleState,
+		public readonly command?: vscode.Command
+	) {
+		super(contactId, collapsibleState);
+
+		this.tooltip = `${this.contactId}-${this.contactName}`;
+		this.description = this.contactFullName;
+	}
+
+	iconPath = {
+		light: path.join(__filename, '..', '..', 'resources', 'rocketchat.svg'),
+		dark: path.join(__filename, '..', '..', 'resources', 'rocketchat.svg')
+	};
+
+	contextValue = 'contacts';
+}
--- src/extension.ts
+++ src/extension.ts
@@ -1,14 +1,50 @@
 import * as vscode from 'vscode';
+import { contactsListProvider, Contacts } from './contacts';
 
 export function activate(context: vscode.ExtensionContext) {
 
-    const provider = new ChatViewProvider(context.extensionUri);
+    // vscode.window.createTreeView('contactsList', {
+    //     treeDataProvider: new contactsListProvider("bamba") //vscode.workspace.rootPath
+    // });
 
-    context.subscriptions.push(
-        vscode.window.registerWebviewViewProvider(ChatViewProvider.viewType, provider));
+    const contactsProvider = new contactsListProvider(""); //vscode.workspace.rootPath
+	vscode.window.registerTreeDataProvider('contactsList', contactsProvider);
+	vscode.commands.registerCommand('contactsList.refreshEntry', () => contactsProvider.refresh());
 
+
+    // const provider = new ChatViewProvider(context.extensionUri);
+    // context.subscriptions.push(
+    //     vscode.window.registerWebviewViewProvider(ChatViewProvider.viewType, provider),
+    //     // vscode.commands.registerCommand("extension.chat.openChatPanel", openChatWebview),
+    // );        
 
 }
+
+
+// const openChatWebview = async (chatArgs?: ChatArgs) => {
+//     let provider = !!chatArgs ? chatArgs.providerName : undefined;
+//     let channelId = !!chatArgs ? chatArgs.channelId : undefined;
+//     const source = !!chatArgs ? chatArgs.source : EventSource.command;
+
+//     if (!chatArgs) {
+//         const selected = await askForChannel(undefined);
+
+//         if (!!selected) {
+//             provider = selected.providerName;
+//             channelId = selected.channel.id;
+//         }
+//     }
+
+//     if (!!provider && !!channelId) {
+//         controller.updateCurrentState(provider, channelId, source);
+//         controller.loadUi();
+
+//         await setup(true, undefined);
+//         await manager.updateWebviewForProvider(provider, channelId);
+//         telemetry.record(EventType.viewOpened, source, channelId, provider);
+//         manager.loadChannelHistory(provider, channelId);
+//     }
+// };
 
 class ChatViewProvider implements vscode.WebviewViewProvider {
     public static readonly viewType = 'chat.viewType';
