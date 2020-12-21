8758f7257a76bed56c483543ee3faa68165dc9db
AlexAvizov <alex.avizov@sap.com>
2020-12-21 21:47:21 +0200

    add buttons

--- src/extension.ts
+++ src/extension.ts
@@ -2,9 +2,16 @@ import * as vscode from 'vscode';
 import { contactsListProvider, ContactsTI } from './contacts';
 
 export function activate(context: vscode.ExtensionContext) {
-
     vscode.window.createTreeView('contactsList', {
         treeDataProvider: new contactsListProvider("bamba") //vscode.workspace.rootPath
     });
+    vscode.commands.registerCommand('contactsList.openChat', cbOpenChat);
+    vscode.commands.registerCommand('contactsList.openChannelChat', cbOpenChannelChat);
+}
+function cbOpenChat (node: ContactsTI) {
+    vscode.window.showInformationMessage(`Successfully called open chat ` + node.label)
+}
 
+function cbOpenChannelChat (node: ContactsTI) {
+    vscode.window.showInformationMessage(`Successfully called open channel chat ` + node.label)
 }
